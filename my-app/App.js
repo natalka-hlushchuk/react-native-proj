0fd8e0b522429e3f57d559b3090b45d5816bf759
Natalka Hlushchuk <hlushchuk.nataliia@gmail.com>
2023-03-03 22:51:40 +0100

    hw2

--- my-app/App.js
+++ my-app/App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, ImageBackground, StyleSheet, View } from "react-native";
+import RegisrationScreen from "./Screens/RegistrationScreen";
+// import * as Font from "expo-font";
+// import RegisrationScreen from "./Screens/RegistrationScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        style={styles.image}
+        source={require("../my-app/assets/bg.png")}
+      >
+        <RegisrationScreen />
+      </ImageBackground>
     </View>
   );
 }
@@ -14,7 +20,45 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+  },
+  form: {
+    backgroundColor: "#FFFFFF",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingRight: 16,
+    paddingLeft: 16,
+  },
+  input: {
+    backgroundColor: "#F6F6F6",
+    padding: 16,
+    borderColor: "#E8E8E8",
+    borderStyle: "solid",
+    borderRadius: 8,
+    borderWidth: 1,
+    color: "#212121",
+    height: 50,
+  },
+
+  formTitle: {
+    paddingTop: 92,
+    paddingBottom: 33,
+    fontSize: 30,
+    textAlign: "center",
+    fontWeight: 500,
+    lineHeight: 1.16,
+    letterSpacing: 0.01,
+  },
+  inputTitle: {
+    color: "#f0f8ff",
+    marginBottom: 10,
+    fontSize: 18,
+  },
+  button: {
+    paddingTop: 43,
   },
 });
---
+++ my-app/Screens/LoginScreen.js
@@ -0,0 +1,113 @@
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+export default function LoginScreen() {
+  return (
+    <View style={styles.form}>
+      <Text style={styles.formTitle}>Ввійти</Text>
+      <View style={{ gap: 16 }}>
+        <TextInput
+          style={styles.input}
+          placeholder={"Адреса електронної пошти"}
+          placeholderTextColor={"#BDBDBD"}
+        />
+        <View style={styles.inputWrap}>
+          <TextInput
+            style={styles.input}
+            placeholder={"Пароль"}
+            placeholderTextColor={"#BDBDBD"}
+          />
+          <Text style={styles.show}>Показати</Text>
+        </View>
+      </View>
+      <View style={{ paddingTop: 43, paddingBottom: 16 }}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.textButton}>Зареєструватися</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.textLogWrap}>
+        <Text style={styles.textLog}> Немає акаунта? Зареєструватися</Text>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  form: {
+    backgroundColor: "#FFFFFF",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingTop: 92,
+    paddingRight: 16,
+    paddingLeft: 16,
+    paddingBottom: 78,
+  },
+  photoWrap: {
+    // width: 120,
+    // height: 120,
+    // backgroundColor: "#F6F6F6",
+    // borderRadius: 16,
+    // // position: "absolute",
+    // justifyContent: "center",
+  },
+  // titleWrap: {  },
+  formTitle: {
+    paddingBottom: 32,
+    fontSize: 30,
+    textAlign: "center",
+    fontWeight: "500",
+    letterSpacing: 0.01,
+    color: "#212121",
+  },
+  input: {
+    backgroundColor: "#F6F6F6",
+    padding: 16,
+    borderColor: "#E8E8E8",
+    borderStyle: "solid",
+    borderRadius: 8,
+    borderWidth: 1,
+    color: "#212121",
+    height: 50,
+  },
+
+  inputTitle: {
+    color: "#f0f8ff",
+    marginBottom: 10,
+    fontSize: 18,
+  },
+  inputWrap: {},
+  show: {
+    position: "absolute",
+    right: 16,
+    top: 13,
+    fontWeight: "400",
+    fontSize: 16,
+    textAlign: "right",
+    color: "#1B4371",
+  },
+  button: {
+    alignItems: "center",
+    paddingTop: 16,
+    paddingRight: 32,
+    paddingBottom: 16,
+    paddingLeft: 32,
+    gap: 12,
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+  },
+  textButton: {
+    color: "#FFFFFF",
+    fontSize: 16,
+  },
+  textLogWrap: {},
+  textLog: {
+    fontWeight: "400",
+    fontSize: 16,
+    textAlign: "center",
+    color: "#1B4371",
+  },
+});
